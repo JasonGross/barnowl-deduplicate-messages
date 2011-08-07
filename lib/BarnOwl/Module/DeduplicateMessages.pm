40f4a6dc756c7bfdd0fc63e1057d12fc9c9e18bf
Jason Gross <jgross@mit.edu>
2011-08-07 06:04:48 -0400

    Updated with daviden's suggestions to be per-class/instance
    Also, only deduplicate the last message to the context.
    
    keep-first currently errors with
       OWL ADMIN
          Internal error! Non-UTF-8 string encountered:
          ...
          Aug  7 06:02:01 2011
    
    I think this is a result of some string getting freed before it should.

--- lib/BarnOwl/Module/DeduplicateMessages.pm
+++ lib/BarnOwl/Module/DeduplicateMessages.pm
@@ -15,40 +15,13 @@ package BarnOwl::Module::DeduplicateMessages;
 
 our $VERSION = 0.1;
 
-my %message_cache = ();
+my $message_cache = {};
 
 my $new_variable_enum;
 if (defined &BarnOwl::new_variable_enum) { # XXX Doesn't exist in 1.8.  Remove when 1.9 becomes standard.
     $new_variable_enum = \&BarnOwl::new_variable_enum;
 } else {
     $new_variable_enum = \&BarnOwl::new_variable_string;
-}
-
-=head2 duplicate-message-display
-
-This BarnOwl variable controls how much time, in minutes, must elapse after a
-particular message before a duplicate of that message can be displayed.
-
-=cut
-
-BarnOwl::new_variable_int('duplicate-message-delay',
-    {
-        default => 0,
-        summary => 'Minimum duration, in minutes, between duplicate messages'
-    });
-
-=head2 clear_message_cache TIME
-
-Clears the cache of messages, to deduplicate, of all messages which arrived
-before C<TIME>.
-
-=cut
-
-sub clear_message_cache {
-    my ($min_time) = @_;
-    foreach my $time (keys %message_cache) {
-        delete $message_cache{$time} if $time < $min_time;
-    }
 }
 
 =head2 duplicate-message-format
@@ -108,48 +81,35 @@ sub on_receive_message {
     my ($m) = @_;
     my $format = BarnOwl::getvar('duplicate-message-format');
     my $time = time;
+    my ($class, $instance) = ($m->class, $m->instance);
     return if $format eq 'keep-all';
-    $message_cache{$time} = [] unless defined $message_cache{$time};
-    clear_message_cache(time - 60 * BarnOwl::getvar('duplicate-message-delay'));
-    foreach my $message_list_ref (values %message_cache) {
-        foreach my $old_m (@$message_list_ref) {
-            next unless defined $old_m;
-            if (are_messages_equal($m, $old_m)) {
-                if ($format eq 'keep-first') {
-                    $m->delete_and_expunge;
-                    push @{$message_cache{$time}}, $old_m;
-                } elsif ($format eq 'keep-last') {
-                    $old_m->delete_and_expunge;
-                    undef $old_m;
-                    push @{$message_cache{$time}}, $m;
-                } elsif ($format eq 'keep-fist-and-last') {
-                    $old_m->delete_and_expunge unless $old_m->{"__deduplicate_messages_first"};
-                    undef $old_m;
-                    push @{$message_cache{$time}}, $m;
-                } else {
-                    die "Invalid setting `$format' for variable duplicate-message-format.\n";
-                }
-                return;
-            }
+    return unless $m->is_zephyr;
+    my $old_m = $message_cache->{$class}->{$instance};
+    if (defined $old_m && are_messages_equal($m, $old_m)) {
+        if ($format eq 'keep-first') {
+            $m->delete_and_expunge;
+        } elsif ($format eq 'keep-last') {
+            $old_m->delete_and_expunge;
+            $message_cache->{$class}->{$instance} = $m;
+        } elsif ($format eq 'keep-first-and-last') {
+            $old_m->delete_and_expunge unless $old_m->{"__deduplicate_messages_first"};
+            $message_cache->{$class}->{$instance} = $m;
+        } else {
+            die "Invalid setting `$format' for variable duplicate-message-format.\n";
         }
+        return;
     }
     $m->{"__deduplicate_messages_first"} = 1; # XXX Hack
-    push @{$message_cache{$time}}, $m;
+    $message_cache->{$class}->{$instance} = $m;
 }
 
-my @methods = qw(type direction body sender recipient login is_private is_login
-                 is_logout is_loginout is_incoming is_outgoing is_admin is_meta
-                 is_generic is_zephyr is_aim is_jabber is_icq is_yahoo is_msn
-                 is_loopback is_ping is_mail is_personal class instance realm
-                 opcode header host hostname auth fields zsig zwriteline
-                 login_host login_tty);
+my @fields = qw(type direction body sender recipient login private
+                class instance realm opcode hostname auth);
 # This is very hackish.  I don't like it.
 sub are_messages_equal {
     my ($m1, $m2) = @_;
-    foreach my $method (@methods) {
-        my ($meth1, $meth2) = (\&{"$m1->$method"}, \&{"$m2->$method"});
-        next unless defined $meth1 && defined $meth2;
-        my ($val1, $val2) = ($meth1->(), $meth2->());
+    foreach my $field (@fields) {
+        my ($val1, $val2) = ($m1->{$field}, $m2->{$field});
         return 0 unless (defined $val1 == defined $val2) && ($val1 eq $val2);
     }
     return 1;
